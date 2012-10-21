b2768ffbe66e2c733a7f61f98de77a53caa87bfb
Derek P <derek@derekperez.com>
2012-10-21 14:08:54 -0700

    adds redis monitoring support to riemann-tools.

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,15 +2,15 @@ require 'rubygems'
 require 'rubygems/package_task'
 require 'rdoc/task'
 require 'find'
- 
+
 # Don't include resource forks in tarballs on Mac OS X.
 ENV['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
 ENV['COPYFILE_DISABLE'] = 'true'
- 
+
 # Gemspec
 gemspec = Gem::Specification.new do |s|
   s.rubyforge_project = 'riemann-tools'
- 
+
   s.name = 'riemann-tools'
   s.version = '0.0.8'
   s.author = 'Kyle Kingsbury'
@@ -23,22 +23,23 @@ gemspec = Gem::Specification.new do |s|
   s.add_dependency 'trollop', '>= 1.16.2'
   s.add_dependency 'munin-ruby', '>= 0.2.1'
   s.add_dependency 'yajl-ruby', '>= 1.1.0'
+  s.add_dependency 'redis', '>= 3.0.2'
 
   s.files = FileList['lib/**/*', 'bin/*', 'LICENSE', 'README.markdown'].to_a
   s.executables |= Dir.entries('bin/')
   s.require_path = 'lib'
   s.has_rdoc = true
- 
+
   s.required_ruby_version = '>= 1.8.7'
 end
 
 Gem::PackageTask.new gemspec do |p|
 end
- 
+
 RDoc::Task.new do |rd|
   rd.main = 'Riemann Tools'
   rd.title = 'Riemann Tools'
   rd.rdoc_dir = 'doc'
- 
+
   rd.rdoc_files.include('lib/**/*.rb')
 end
