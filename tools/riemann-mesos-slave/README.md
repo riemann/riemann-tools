481b0bfdc964c49d96a8fe0d389223dede462569
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-02-10 14:28:10 +0100

    Riemann tool to monitor Mesos slaves
    Contrary to riemann-mesos, this is meant to be run on every single mesos slave.

---
+++ tools/riemann-mesos-slave/README.md
@@ -0,0 +1,11 @@
+# Riemann Mesos
+
+Gathers Mesos Slave metrics and sends them to Riemann. In general this should
+be run on every single slave node.
+
+# Getting started
+
+```
+gem install riemann-mesos-slave
+riemann-mesos-slave --help
+```
---
+++ tools/riemann-mesos-slave/Rakefile.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'rubygems/package_task'
+require 'rdoc/task'
+require 'find'
+
+# Don't include resource forks in tarballs on Mac OS X.
+ENV['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
+ENV['COPYFILE_DISABLE'] = 'true'
+
+# Gemspec
+gemspec = Gem::Specification.new do |s|
+  s.rubyforge_project = 'riemann-mesos-slave'
+
+  s.name = 'riemann-mesos-slave'
+  s.version = '0.1.1'
+  s.author = 'Giulio Eulisse'
+  s.email = 'giulio.eulisse@cern.ch'
+  s.homepage = 'https://github.com/riemann/riemann-tools'
+  s.platform = Gem::Platform::RUBY
+  s.summary = 'Submits mesos slave stats to riemann.'
+  s.license = 'MIT'
+
+  s.add_dependency 'riemann-tools', '>= 0.2.8'
+  s.add_dependency 'faraday', '>= 0.8.5'
+  s.add_dependency 'json'
+
+  s.files = FileList['bin/*', 'LICENSE', 'README.md'].to_a
+  s.executables |= Dir.entries('bin/')
+  s.has_rdoc = false
+
+  s.required_ruby_version = '>= 1.8.7'
+end
+
+Gem::PackageTask.new gemspec do |p|
+end
