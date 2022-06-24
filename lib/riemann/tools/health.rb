070d9349b611830cd82addcd5882772763d9ec57
Romain Tartière <romain@blogreen.org>
2022-06-24 09:27:56 -1000

    Fix Lint/AssignmentInCondition

--- lib/riemann/tools/health.rb
+++ lib/riemann/tools/health.rb
@@ -247,7 +247,7 @@ module Riemann
             @topdata[:load] = Regexp.last_match(1).to_f
           elsif ln.match(/CPU usage: [0-9.]+% user, [0-9.]+% sys, ([0-9.]+)% idle/i)
             @topdata[:cpu] = 1 - (Regexp.last_match(1).to_f / 100)
-          elsif mdat = ln.match(/PhysMem: ([0-9]+)([BKMGT]) wired, ([0-9]+)([BKMGT]) active, ([0-9]+)([BKMGT]) inactive, ([0-9]+)([BKMGT]) used, ([0-9]+)([BKMGT]) free/i)
+          elsif (mdat = ln.match(/PhysMem: ([0-9]+)([BKMGT]) wired, ([0-9]+)([BKMGT]) active, ([0-9]+)([BKMGT]) inactive, ([0-9]+)([BKMGT]) used, ([0-9]+)([BKMGT]) free/i))
             wired = mdat[1].to_i * (1024**'BKMGT'.index(mdat[2]))
             active = mdat[3].to_i * (1024**'BKMGT'.index(mdat[4]))
             inactive = mdat[5].to_i * (1024**'BKMGT'.index(mdat[6]))
@@ -257,7 +257,7 @@ module Riemann
           # This is for OSX Mavericks which
           # uses a different format for top
           # Example: PhysMem: 4662M used (1328M wired), 2782M unused.
-          elsif mdat = ln.match(/PhysMem: ([0-9]+)([BKMGT]) used \([0-9]+[BKMGT] wired\), ([0-9]+)([BKMGT]) unused/i)
+          elsif (mdat = ln.match(/PhysMem: ([0-9]+)([BKMGT]) used \([0-9]+[BKMGT] wired\), ([0-9]+)([BKMGT]) unused/i))
             used = mdat[1].to_i * (1024**'BKMGT'.index(mdat[2]))
             unused = mdat[3].to_i * (1024**'BKMGT'.index(mdat[4]))
             @topdata[:memory] = used.to_f / (used + unused)
