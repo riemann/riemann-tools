300d61095c2eb600d2219b6617e8fba17f621edb
James Turnbull <james@lovedthanlost.net>
2022-06-24 22:46:14 -0400

    Modernized riemann-ntp and included warning for macOS
    Signed-off-by: James Turnbull <james@lovedthanlost.net>

---
+++ lib/riemann/tools/ntp.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'riemann/tools'
+
+module Riemann
+    module Tools
+      class Ntp
+        include Riemann::Tools
+  
+        def initialize
+          @hostname = `hostname`.chomp
+          @ostype = `uname -s`.chomp.downcase
+          if @ostype = 'darwin'
+            puts "WARNING: macOS not explicitly supported. Exiting."
+            exit 1        
+          end
+        end
+  
+        def tick
+          stats = `ntpq -p -n`
+          stats.each_line do |stat|
+            m = stat.split
+            next if m.grep(/^===/).any? || m.grep(/^remote/).any?
+  
+            @ntp_host = m[0].gsub('*', '').gsub('-', '').gsub('+', '')
+            send('delay', m[7])
+            send('offset', m[8])
+            send('jitter', m[9])
+          end
+        end
+  
+        def send(type, metric)
+          report(
+            host: @hostname,
+            service: "ntp peer #{@ntp_host} #{type}",
+            metric: metric.to_f,
+            state: 'ok',
+            description: "ntp peer #{@ntp_host} #{type}",
+            tags: ['ntp'],
+          )
+        end
+      end
+    end
+  end
