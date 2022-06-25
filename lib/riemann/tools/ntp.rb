500164f0b5b773509a4e369bb40b48d47dd38dde
James Turnbull <james@lovedthanlost.net>
2022-06-24 23:14:00 -0400

    rubocop
    Signed-off-by: James Turnbull <james@lovedthanlost.net>

--- lib/riemann/tools/ntp.rb
+++ lib/riemann/tools/ntp.rb
@@ -16,25 +16,26 @@ module Riemann
       def tick
         stats = `ntpq -p -n`
         stats.each_line do |stat|
-        m = stat.split
-        next if m.grep(/^===/).any? || m.grep(/^remote/).any?
-  
-        @ntp_host = m[0].gsub('*', '').gsub('-', '').gsub('+', '')
-        send('delay', m[7])
-        send('offset', m[8])
-        send('jitter', m[9])
-      end
-    end
+          m = stat.split
+          next if m.grep(/^===/).any? || m.grep(/^remote/).any?
 
-    def send(type, metric)
-      report(
-        host: @hostname,
-        service: "ntp peer #{@ntp_host} #{type}",
-        metric: metric.to_f,
-        state: 'ok',
-        description: "ntp peer #{@ntp_host} #{type}",
-        tags: ['ntp'],
-      )
+          @ntp_host = m[0].gsub('*', '').gsub('-', '').gsub('+', '')
+          send('delay', m[7])
+          send('offset', m[8])
+          send('jitter', m[9])
+        end
+      end
+
+      def send(type, metric)
+        report(
+          host: @hostname,
+          service: "ntp peer #{@ntp_host} #{type}",
+          metric: metric.to_f,
+          state: 'ok',
+          description: "ntp peer #{@ntp_host} #{type}",
+          tags: ['ntp'],
+        )
+      end
     end
   end
 end
